7bd072b41817267ee869797ad43fe54e1ed5dbfd
Shun Yao <ysmiracle@hotmail.com>
2018-05-06 19:00:47 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @item = cart
+    #@item = cart
   end
 
   def add
     cart << params[:name]
-    @item = cart
-    render :index
-    @redirect_to products_path
+    #@item = cart
+    #render :index
+    redirect_to products_path
   end
 end
